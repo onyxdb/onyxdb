3f8822be8c1fb4bf225f98ce262edb0f726059e6
Sergey Mokhov <foxleren@gmail.com>
2024-11-01 06:51:40 +0300

    .

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,12 +10,3 @@ dependencies {
     // Deps for onyxdb-java-spring-app-conventions plugin
     implementation(libs.springBoot.gradlePlugin)
 }
-
-gradlePlugin {
-    plugins {
-        create("xor-openapi-conventions") {
-            id = "xor-openapi-conventions"
-            implementationClass = "ru.xority.conventions.openapi.XorOpenapiConventionsPlugin"
-        }
-    }
-}
