4a255297536d593383a57e5ae1d8d03de00a3709
Sergey Mokhov <foxleren@gmail.com>
2024-11-03 06:32:46 +0300

    .

---
+++ api/src/main/java/com/onyxdb/api/controllers/ClustersController.java
@@ -0,0 +1,18 @@
+package com.onyxdb.api.controllers;
+
+import com.onyxdb.api.generated.apis.OnyxApiV1Api;
+import com.onyxdb.api.generated.models.CreateClusterRequest;
+import com.onyxdb.api.generated.models.CreateClusterResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author foxleren
+ */
+@RestController
+public class ClustersController implements OnyxApiV1Api {
+    @Override
+    public ResponseEntity<CreateClusterResponse> apiV1CreateCluster(CreateClusterRequest createClusterRequest) {
+        return null;
+    }
+}
