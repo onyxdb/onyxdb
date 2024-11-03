4e4faa52d960aba3d439954c64e757b5e686fd17
Sergey Mokhov <foxleren@gmail.com>
2024-11-03 06:41:16 +0300

    Rename tag in openapi scheme (#8)

--- api/src/main/java/com/onyxdb/api/controllers/ClustersController.java
+++ api/src/main/java/com/onyxdb/api/controllers/ClustersController.java
@@ -1,6 +1,6 @@
 package com.onyxdb.api.controllers;
 
-import com.onyxdb.generated.api.apis.OnyxApiV1Api;
+import com.onyxdb.generated.api.apis.OnyxApiV1ClustersApi;
 import com.onyxdb.generated.api.models.CreateClusterRequest;
 import com.onyxdb.generated.api.models.CreateClusterResponse;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author foxleren
  */
 @RestController
-public class ClustersController implements OnyxApiV1Api {
+public class ClustersController implements OnyxApiV1ClustersApi {
     @Override
     public ResponseEntity<CreateClusterResponse> apiV1CreateCluster(CreateClusterRequest createClusterRequest) {
         return null;
