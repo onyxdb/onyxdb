49ba1b912a225463892e6c7ac383214b784808dd
Sergey Mokhov <foxleren@gmail.com>
2024-11-10 10:37:25 +0300

    .

---
+++ mongodb-operator/src/test/java/com/onyxdb/mongodbOperator/MongodbOperatorApplicationTests.java
@@ -0,0 +1,11 @@
+package com.onyxdb.mongodbOperator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbOperatorApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,4 @@ rootProject.name = "onyxdb"
 include(":common")
 include(":common:postgres")
 include(":mdb")
+include(":mongodb-operator")
