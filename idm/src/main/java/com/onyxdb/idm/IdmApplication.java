0d5aa4a14d2ef844a15351fbede8bd8cd1b3abf8
Sergey Mokhov <foxleren@gmail.com>
2025-01-09 01:26:17 +0300

    .

--- idm/src/main/java/com/onyxdb/idm/IdmApplication.java
+++ idm/src/main/java/com/onyxdb/idm/IdmApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IdmApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(IdmApplication.class, args);
     }
-
 }
