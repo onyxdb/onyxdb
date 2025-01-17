8deb3cc5d4cb6c45ab7b2a7ac70d6101935e63d9
SirDaukar <artemka.fedorov@gmail.com>
2025-01-18 01:50:09 +0300

    init working graphql schema

--- idm/build.gradle.kts
+++ idm/build.gradle.kts
@@ -1,13 +1,45 @@
 plugins {
     alias(libs.plugins.onyxdb.javaSpringAppConventions)
+    alias(libs.plugins.onyxdb.jooqManualConventions)
+    alias(libs.plugins.lombok)
+
+    id("io.spring.dependency-management")
+    id("com.netflix.dgs.codegen") version "7.0.3"
 }
 
 configurations.all {
     exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
 }
 
+extra["netflixDgsVersion"] = "10.0.1"
+
 dependencies {
+    implementation(project(":common:postgres"))
+
+//    NETFLIX START
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.netflix.graphql.dgs:graphql-dgs-spring-graphql-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.netflix.graphql.dgs:graphql-dgs-spring-graphql-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+//    NETFLIX END
+
     implementation(libs.springBoot.starterWeb)
     implementation(libs.springBoot.starterLog4j2)
+
     testImplementation(libs.springBoot.starterTest)
+    testImplementation(libs.testcontainers.junitJupiter)
+    testImplementation(libs.testcontainers.postgresql)
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:${property("netflixDgsVersion")}")
+    }
+}
+
+tasks.generateJava {
+    schemaPaths.add("${projectDir}/src/main/resources/schema")
+    packageName = "com.onyxdb.idm.codegen"
+    generateClient = true
 }
---
+++ idm/src/main/java/com/onyxdb/idm/v1/V1PingController.java
@@ -0,0 +1,19 @@
+package com.onyxdb.idm.v1;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author foxleren
+ */
+@RestController
+@RequiredArgsConstructor
+public class V1PingController {
+
+    @GetMapping(path = "/ping")
+    public ResponseEntity<String> v1ClustersCreateCluster() {
+        return ResponseEntity.ok("OK");
+    }
+}
