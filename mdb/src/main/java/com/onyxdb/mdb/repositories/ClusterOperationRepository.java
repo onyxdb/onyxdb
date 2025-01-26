9bad121d772d5bc8dfb592282891efb23170576c
Sergey Mokhov <foxleren@gmail.com>
2025-01-27 00:08:06 +0300

    refactoring models

--- mdb/src/main/java/com/onyxdb/mdb/repositories/ClusterOperationRepository.java
+++ mdb/src/main/java/com/onyxdb/mdb/repositories/ClusterOperationRepository.java
@@ -1,7 +1,5 @@
 package com.onyxdb.mdb.repositories;
 
-import java.util.List;
-
 import com.onyxdb.mdb.models.ClusterOperation;
 
 /**
--- mdb/src/main/java/com/onyxdb/mdb/services/ClusterOperationService.java
+++ mdb/src/main/java/com/onyxdb/mdb/services/ClusterOperationService.java
@@ -1,11 +1,8 @@
 package com.onyxdb.mdb.services;
 
-import java.util.List;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import com.onyxdb.mdb.models.ClusterOperation;
 import com.onyxdb.mdb.repositories.ClusterOperationRepository;
 
 /**
