bff18570dee8b0686835933cd70550337c344927
Sergey Mokhov <foxleren@gmail.com>
2025-03-20 16:08:55 +0300

    .

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,6 @@ rootProject.name = "onyxdb"
 
 include(":common")
 include(":common:postgres")
+
 include(":mdb")
-include(":mongodb-k8s-operator")
 include(":idm")
-include("common:redis")
-findProject(":common:redis")?.name = "redis"
