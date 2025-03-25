449a4cff39e9ea9fea9ca4bb17b48d5bb7e68058
Sergey Mokhov <foxleren@gmail.com>
2025-03-25 04:34:57 +0300

    .

--- mdb/src/main/java/com/onyxdb/mdb/configurations/RepositoryConfiguration.java
+++ mdb/src/main/java/com/onyxdb/mdb/configurations/RepositoryConfiguration.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Configuration;
 
 import com.onyxdb.mdb.core.hardwarePresets.HardwarePresetPostgresRepository;
 import com.onyxdb.mdb.core.hardwarePresets.HardwarePresetRepository;
+import com.onyxdb.mdb.core.projects.ProjectPostgresRepository;
+import com.onyxdb.mdb.core.projects.ProjectRepository;
 import com.onyxdb.mdb.core.zones.ZonePostgresRepository;
 import com.onyxdb.mdb.core.zones.ZoneRepository;
-import com.onyxdb.mdb.repositories.ProjectPostgresRepository;
-import com.onyxdb.mdb.repositories.ProjectRepository;
 
 /**
  * @author foxleren
