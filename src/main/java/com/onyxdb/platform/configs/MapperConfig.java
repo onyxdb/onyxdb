cfd74d9d5bb004c5231574ccbbfdf54299cea293
foxleren <foxleren@gmail.com>
2025-04-23 22:31:54 +0300

    .

--- src/main/java/com/onyxdb/platform/configs/MapperConfig.java
+++ src/main/java/com/onyxdb/platform/configs/MapperConfig.java
@@ -2,6 +2,7 @@ package com.onyxdb.platform.configs;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
@@ -25,6 +26,7 @@ public class MapperConfig {
     public ObjectMapper objectMapper() {
         return JsonMapper.builder()
                 .addModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                 .build();
     }
